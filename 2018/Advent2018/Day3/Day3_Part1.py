5051226e39bf70711a791967e007b3fc9ec47e35
Josh Gold <killercatfish@gmail.com>
2018-12-03 11:16:05 -0500

    parsed input

--- 2018/Advent2018/Day3/Day3_Part1.py
+++ 2018/Advent2018/Day3/Day3_Part1.py
@@ -1,5 +1,25 @@
 
+'''
+a list with lists of the integer values of the inputs
+[claim ID, left edge, right edge, width, height]
+'''
+input_list = []
 
-with open("../input/inputday2.txt") as f:
+with open("text.txt") as f:
     for line in f:
         l = line.strip()
+        j = l[1:]
+        j = j.replace(' @ ',',')
+        j = j.replace(': ', ',')
+        j = j.replace('x', ',')
+        new_entry = [int(x) for x in j.split(',')]
+
+        input_list.append(new_entry)
+
+
+locations = {}
+
+for i in input_list:
+    
+
+print(input_list)
