7e356c85682981056e701bd8eeba0b743ac6447a
Josh Gold <killercatfish@gmail.com>
2018-12-04 16:03:11 -0500

    working on day 4 still

--- 2018/Advent2018/Day4/day4_part1.py
+++ 2018/Advent2018/Day4/day4_part1.py
@@ -40,6 +40,8 @@ schedule.append(["Date","ID","Minute"])
 schedule.append(["","",tens])
 schedule.append(["","",ones])
 
+awake_list = '.' * 60
+
 # print(schedule[0])
 # print(schedule[1])
 # print(schedule[2])
@@ -52,44 +54,64 @@ def RepresentsInt(s):
     except ValueError:
         return False
 
+id_list = []
+
 #This did nicely for getting dates and id's.  But I am going to use a while loop for real processing
-    
-# for i in range(len(input_list)):
-#     ID = input_list[i][1].split(" ")[1][1:]
-#     DATE = input_list[i][0][:5]
-#     TIME = input_list[i][0][6:8]
-#     if RepresentsInt(ID):
-#         #print("ID is an int: ", ID)
-#         #new_entry = "%-*s%-*s" % (8, i[0][:6], 8, ID)
-#         if int(TIME) != 0:
-#             print(TIME)
-#             DATE = input_list[i+1][0][:5]
-#
-#         schedule.append([DATE,ID,""])
+
+for i in range(len(input_list)):
+    ID = input_list[i][1].split(" ")[1][1:]
+    DATE = input_list[i][0][:5]
+    TIME = input_list[i][0][6:8]
+    if RepresentsInt(ID):
+        id_list.append(ID)
+        #print("ID is an int: ", ID)
+        #new_entry = "%-*s%-*s" % (8, i[0][:6], 8, ID)
+        if int(TIME) != 0:
+            DATE = input_list[i+1][0][:5]
+
+        schedule.append([DATE,ID,awake_list])
+
+def determine(x):
+    #print(x[1].split(" ")[1][1:])
+    return RepresentsInt(x[1].split(" ")[1][1:])
+
+# https://stackoverflow.com/questions/1207406/how-to-remove-items-from-a-list-while-iterating
+input_list[:] = [x for x in input_list if not determine(x)]
 
 
-'''
-Going to make it so that each entry is separate item in list
-'''
-# for i in input_list:
-#     print(i[1].split(" "))
-#     ID = i[1].split(" ")[1][1:]
-#     if RepresentsInt(ID):
-#         #print("ID is an int: ", ID)
-#         new_entry = "%-*s%-*s" % (8, i[0][:6], 8, ID)
-#         schedule.append(new_entry)
-#     # else:
-#     #     print("ID is not an int: ", ID)
-#     #
-#     # if i[0][:6] != schedule[len(schedule)-1][:6]:
-#     #
-#     #     new_entry = "%-*s%-*s" %(8,i[0][:6],8,i[0][6:])
-#     #     schedule.append(new_entry)
+print(id_list)
+print("****")
 
-for i in schedule:
-    print(i)
+actions_dict = {}
+AWAKE = True
 
-i = 0
-while i < len(input_list):
-    print(i)
-    i += 15
+for i in input_list:
+    DATE = i[0][:5]
+
+    if DATE in actions_dict.keys():
+        sleep_pattern = actions_dict[DATE]
+        if AWAKE:
+            sleep_pattern[1].append(int(i[0][-2:]))
+        else:
+            sleep_pattern[0].append(int(i[0][-2:]))
+        AWAKE = not AWAKE
+    else:
+        actions_dict[DATE] = [[],[],id_list[0]] #[awakes,asleeps] times
+        id_list = id_list[1:]
+
+print(actions_dict)
+
+def print_actions():
+    heading = "%-*s%-*s%s" % (8,"Date",8,"ID","Minute")
+    tens_heading = "%-*s%-*s%s" % (8,"",8,"",tens)
+    ones_heading = "%-*s%-*s%s" % (8,"",8,"",ones)
+    print(heading)
+    print(tens_heading)
+    print(ones_heading)
+    for i in actions_dict:
+       print("%-*s%-*s%s" % (8,i,8,actions_dict[i][2],actions_dict[i][1]))
+
+print_actions()
+
+# for i in schedule:
+#     print("%-*s%-*s%s" % (8,i[0],8,i[1],i[2]))
