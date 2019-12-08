2cb08b89f97e56e2370ed3b6ce54787597c425f5
Josh Gold <killercatfish@gmail.com>
2019-12-07 19:05:01 -0500

    Part 1 works, Cleaning up mess, placed Int_Code_Computer in Intcode.py

--- 2019/Day7/Intcode.py
+++ 2019/Day7/Intcode.py
@@ -1,20 +1,3 @@
-'''
-AoC 2019 7.1
-'''
-
-# get puzzle input
-def get_input(file_name):
-    input = []
-
-    with open(f'input/{file_name}.txt'.format(file_name, 'wb')) as f:
-        # with open("input/day5_test_input.txt") as f:
-        #     input = f.split(',')
-        for line in f:
-            l = line.strip()
-            # print(l)
-            input = [int(n) for n in l.split(',')]
-    return input
-
 class Int_Code_Computer():
     def __init__(self, input):
         self.instructions = []
@@ -172,44 +155,3 @@ class Int_Code_Computer():
         # print(self.output)
         self.input = input
         return self.output
-
-input = get_input('day_7_input')
-
-from itertools import permutations
-codes = [0, 1, 2, 3, 4]
-perms = list(permutations(codes,5))
-
-# signals = []
-# run = 1
-# for i in perms:
-#     # print("**RUN #" + str(run) + "**")
-#     # print("Perm: " + str(i))
-#     signal = 0
-#     result = 0
-#     for j in range(len(i)):
-#         int_comp = Int_Code_Computer(input.copy())
-#         instructions = [i[j], int(result)]
-#
-#         result = int_comp.solve_puzzle(instructions)
-#         signal += int(result)
-#     signals.append(signal)
-#     run += 1
-#
-# print(signals)
-# print(max(signals))
-
-
-'''Tests
-Solution: new copy of input each amp, not sharing among iteration
-always give result of previous to next as 2nd input
-'''
-# inp = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
-codes = [4,3,2,1,0]
-sum = 0
-result = 0
-for j in range(len(codes)):
-    int_comp = Int_Code_Computer([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
-    instructions = [codes[j], int(result)]
-    result = int_comp.solve_puzzle(instructions)
-    sum += int(result)
-print("Sum: " + str(sum))
---
+++ 2019/Day7/day7_part1.py
@@ -0,0 +1,57 @@
+'''
+AoC 2019 7.1
+'''
+
+# get puzzle input
+def get_input(file_name):
+    input = []
+
+    with open(f'input/{file_name}.txt'.format(file_name, 'wb')) as f:
+        # with open("input/day5_test_input.txt") as f:
+        #     input = f.split(',')
+        for line in f:
+            l = line.strip()
+            # print(l)
+            input = [int(n) for n in l.split(',')]
+    return input
+
+from Intcode import Int_Code_Computer
+
+input = get_input('../input/day_7_input')
+
+from itertools import permutations
+codes = [0, 1, 2, 3, 4]
+perms = list(permutations(codes,5))
+
+signals = []
+run = 1
+for i in perms:
+    # print("**RUN #" + str(run) + "**")
+    # print("Perm: " + str(i))
+    result = 0
+    for j in range(len(i)):
+        int_comp = Int_Code_Computer(input.copy())
+        instructions = [i[j], int(result)]
+
+        result = int_comp.solve_puzzle(instructions)
+    signals.append(int(result))
+    run += 1
+
+print(signals)
+print(max(signals))
+
+
+'''Tests
+Solution: new copy of input each amp, not sharing among iteration
+always give result of previous to next as 2nd input
+'''
+# inp = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+# codes = [4,3,2,1,0]
+# sum = 0
+# result = 0
+# for j in range(len(codes)):
+#     int_comp = Int_Code_Computer([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+#     instructions = [codes[j], int(result)]
+#     result = int_comp.solve_puzzle(instructions)
+#     sum += int(result)
+# print("Sum: " + result)
